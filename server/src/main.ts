e8444d930a7c3c4062b2d28e60787a3d2134c4a3
Aleksey K <kulgeyko93@mail.ru>
2021-09-02 16:48:51 +0300

    feat: basic structure nestjs

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+const PORT = process.env.PORT || 8888;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('NBA')
+    .setDescription('Nba api when you can get data about nba players')
+    .setVersion('1.0')
+    .addTag('nba')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
 }
 bootstrap();
